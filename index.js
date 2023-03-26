d1c9cadab157ebd2adef52ac63b31d77d32d25aa
yanakin394 <yana.germatskaya@gmail.com>
2023-03-26 11:20:37 +0300

    function added

---
+++ index.js
@@ -0,0 +1,10 @@
+const blockFoto = document.getElementById("img")
+//console.log(blockFoto);
+
+function clickPrevious() {
+    blockFoto.style.backgroundImage = "url('assets/images/cat2.jpg')";
+}
+
+function clickNext() {
+    /* blockFoto.style.backgroundImage = url("assets/images/cat2.jpg") */
+}
